f7d1c5a09a08076af3599a2de1ae3c15e1473518
bot <root@0x.mk>
2022-03-22 17:01:00 +0800

    78f4ff6636ed5780

--- masterdata.db/m_tutorial_package.sql
+++ masterdata.db/m_tutorial_package.sql
@@ -2,26 +2,26 @@ CREATE TABLE m_tutorial_package(
   package TEXT NOT NULL,
   PRIMARY KEY (package)
 );
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"story:MS/001/ms_001_01a");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"story:MS/001/ms_001_01b");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"story-voice:MS/001/ms_001_01a");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"story-voice:MS/001/ms_001_01b");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"music:10001");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"music:11007");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"music:11001");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"music:12001");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1000101");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1000102");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1000103");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100701");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100702");
-INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100703");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100101");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100102");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100103");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100701");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100702");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1100703");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1200101");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1200102");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"live:1200103");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"music:10001");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"music:11001");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"music:11007");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"music:12001");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"story-voice:MS/001/ms_001_01a");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"story-voice:MS/001/ms_001_01b");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"story:MS/001/ms_001_01a");
+INSERT INTO `m_tutorial_package` VALUES (/*package*/"story:MS/001/ms_001_01b");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"suit:102012001");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"suit:102022001");
 INSERT INTO `m_tutorial_package` VALUES (/*package*/"suit:102032001");
